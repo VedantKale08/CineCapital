674ec10d703b13ae29a5c1fb18058cdd30414b7f
Aaditya8C <aadityajp0419@gmail.com>
2024-09-03 23:04:08 +0530

    admin_panel_folder_structure_added

---
+++ frontend/components/admin/auth/Login.jsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <p>Login</p>
+    </div>
+  );
+};
+
+export default Login;
---
+++ frontend/components/admin/auth/Register.jsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const Register = () => {
+  return (
+    <div>
+      <p>Register Admin</p>
+    </div>
+  );
+};
+
+export default Register;
